aa428338e8280bbe50acd419f4b215ff3bdbf512
Chris Antaki <ChrisAntaki@gmail.com>
2014-08-09 13:27:16 -0700

    Displaying posts with isotope

--- js/secret.js
+++ js/secret.js
@@ -1,3 +1,31 @@
-$('.isotope').isotope({
-    //
+$.getJSON("data/posts.json", function (posts) {
+    var $wrapper = $("<div>");
+
+    for (var i in posts) {
+        var post    = posts[i];
+        var caption = post["photo-caption"].trim();
+        var image   = post["photo-url-500"].replace(/^http:\/\/\d+/, "https://38");
+        var imageXL = post["photo-url-1280"].replace(/^http:\/\/\d+/, "https://38");
+
+        var $el = $.template("#template-photo", {
+            "caption" : caption,
+            "image"   : image,
+            "data-xl" : imageXL
+        });
+
+        var height  = (post.height / post.width) * 300;
+        $el.find("img").height(height);
+
+        $wrapper.append($el);
+    }
+
+    $(".isotope").append($wrapper.children());
+
+    $(".isotope").isotope({
+        itemSelector : ".photo",
+        masonry : {
+            columnWidth : 320,
+            isFitWidth  : true
+        }
+    });
 });
---
+++ js/vendor/jquery.template.js
@@ -0,0 +1,28 @@
+/*!
+* @license Copyright 2014 Chris Antaki. MIT License: http://en.wikipedia.org/wiki/MIT_License
+*/
+
+;(function ($) {
+    var templates = {};
+
+    function jQueryDotTemplate (target, values) {
+        if (!templates[target]) {
+            templates[target] = $(target).html();
+        }
+
+        var $html = $('<div>' + templates[target] + '</div>');
+
+        for (var i in values) {
+            $html.find('.attribute-' + i).attr(i, values[i]);
+            $html.find('.class-' + i).addClass(values[i]);
+            $html.find('.href-' + i).attr('href', values[i]);
+            $html.find('.src-' + i).attr('src', values[i]);
+            $html.find('.target-' + i).attr('target', values[i]);
+            $html.find('.text-' + i).text(values[i]);
+        }
+
+        return $html.children();
+    }
+
+    $.template = jQueryDotTemplate;
+})(jQuery);
