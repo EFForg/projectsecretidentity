f1a950a16ba61f86f094b0a91df98694821a01cf
Chris Antaki <ChrisAntaki@gmail.com>
2014-08-27 16:25:49 -0700

    Refactored Join page JS

--- js/join.js
+++ js/join.js
@@ -1,19 +1,28 @@
-var $iframe = $('iframe');
+(function() { // :)
+
+
+// Configure Vex
+vex.defaultOptions.className = 'vex-theme-default';
 vex.dialog.buttons.YES.text = 'Accept';
 vex.dialog.buttons.NO.text = 'Decline';
 
-// Vex
-var hasViewedPrivacyPolicy = false;
-vex.defaultOptions.className = 'vex-theme-default';
-
+// Show prompt
 vex.dialog.confirm({
     message: "Before we the load the Tumblr form, we'd like to make sure you agree with their <a class='tumblr-privacy-policy' href='https://www.tumblr.com/policy/en/privacy' target='_blank'>privacy policy</a>.",
-    callback: function(accepted) {
-        if (accepted) {
-            var url = $iframe.data('src');
-            $iframe.attr('src', url);
-        } else {
+    callback: function (accept) {
+        // If a visitor declines, we'll redirect them
+        if (!accept) {
             window.location = './';
+            return;
         }
+
+        // Activate the iframe
+        var $iframe = $('iframe');
+        var url = $iframe.data('src');
+        $iframe.attr('src', url);
     }
 });
+
+
+
+})(); // :)
